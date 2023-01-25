62e452dbe5fa126b03b53da39485cba645fac001
shuto2121 <114901492+shuto2121@users.noreply.github.com>
2023-01-25 14:11:12 +0900

    Delete src directory

--- src/api.js
+++
@@ -1,7 +0,0 @@
-export async function fetchImages(breed) {
-    const response = await fetch(
-      `https://v6.exchangerate-api.com/v6/6e5b977b2a10cc8cf7a99a53/pair/USD/JPY`
-    );
-    const data = await response.json();
-    return data;
-  }
